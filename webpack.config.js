764a4e76b5de0f7eb38ef831a0a52459117b9a32
Tyler Kelley <tyler@lyveminds.com>
2015-11-02 13:47:14 -0800

    renaming index.js -> example.js for clarity

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     'webpack/hot/dev-server',
     'webpack-dev-server/client?http://localhost:8080',
-    './src/index.js'
+    './src/example.js'
   ],
   output: {
     path: __dirname + '/dist',
