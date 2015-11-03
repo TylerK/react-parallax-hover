0ffe508f26e881b6ca87defa91a3d76fbd846fc5
Tyler Kelley <tylerckelley@gmail.com>
2015-11-02 20:15:17 -0800

    Moving styles into the component

--- src/parallax-hover.js
+++ src/parallax-hover.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import styles from './styles';
 
 const config = {
   scale: 1.03, // How large to scale the item: 1.00 -> 1.10~
   rotation: 0.3, // Rotation modifier: 0.1 (more) -> 0.5 (less)
   alpha: 0.4, // Alpha channel modifer: 1.01 -> 1.1~
   shadow: 8 // How much the shadow moves
+};
+
+const transition = 'all 0.3s ease-out';
+const layersBase = {
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0
+};
+
+const styles = {
+  phOutter: () => ({
+    transformStyle: 'preserve-3d'
+  }),
+  phWrapper: () => ({
+    position: 'relative',
+    transition: transition,
+    margin: 0,
+    padding: 0
+  }),
+  phShadow: () => Object.assign({}, layersBase, {
+    height: '90%',
+    width: '90%',
+    left: '5%',
+    top: '5%',
+    background: 'none',
+    transition: transition
+  }),
+  phLayers: () => Object.assign({}, layersBase, {}),
+  phLayer: () => Object.assign({}, layersBase, {}),
+  phLighting: () => Object.assign({}, layersBase, {
+    backgroundImage: 'linear-gradient(135deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 33%)'
+  }),
+  phText: () => Object.assign({}, layersBase, {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: '5rem',
+    color: '#ffffff'
+  })
 };
 
 export default class ParallaxHover extends React.Component {
